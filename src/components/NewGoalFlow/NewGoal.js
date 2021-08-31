64b8e898023c5b2c2799a12903e22ff6cbc0c617
Whitney <whitzhu@gmail.com>
2021-08-31 10:15:54 -0700

    3years goal add error state

--- src/components/NewGoalFlow/NewGoal.js
+++ src/components/NewGoalFlow/NewGoal.js
@@ -8,6 +8,7 @@ export default function NewGoal({ user }) {
     goalName: "",
     threeYearGoalDescription: "",
   });
+  const [errors, setErrors] = useState({});
   const [goalsData, dispatch] = useContext(GoalsContext);
 
   const handleOnTextFieldChange = (e) => {
@@ -19,22 +20,39 @@ export default function NewGoal({ user }) {
 
   const handleOnNextClick = () => {
     const { goalName, threeYearGoalDescription } = localGoalData;
-    dispatch({
-      type: "updateNewGoal",
-      payload: {
-        newGoal: {
-          ...goalsData.newGoal,
-          goalName,
-          threeYearGoalDescription,
+    if (validateData()) {
+      dispatch({
+        type: "updateNewGoal",
+        payload: {
+          newGoal: {
+            ...goalsData.newGoal,
+            goalName,
+            threeYearGoalDescription,
+          },
         },
-      },
+      });
+      console.log("saveThreeYearGoal");
+      saveThreeYearGoal(user.uid, goalName, threeYearGoalDescription);
+    }
+  };
+
+  const validateData = () => {
+    const newErrors = {};
+    let hasError = false;
+    Object.keys(localGoalData).map((key) => {
+      const isEmpty = localGoalData[key] === "";
+      newErrors[key] = isEmpty;
+      if (isEmpty) {
+        hasError = true;
+      }
     });
-    saveThreeYearGoal(user.uid, goalName, threeYearGoalDescription);
+    setErrors(newErrors);
+    return !hasError;
   };
 
   return (
     <Box display="flex" flexDirection="column" mr={2} ml={2} mt={4} mb={4}>
-      <Box mb={2}>
+      <Box mb={4}>
         <Box mb={1}>
           <Typography variant="h1">New Goal</Typography>
         </Box>
@@ -51,6 +69,7 @@ export default function NewGoal({ user }) {
           variant="outlined"
           placeholder="Give your goal a name"
           value={localGoalData.goalName}
+          error={errors.goalName}
           onChange={handleOnTextFieldChange}
         />
       </Box>
@@ -65,6 +84,7 @@ export default function NewGoal({ user }) {
           variant="outlined"
           placeholder="Describe your goal and why is this important"
           value={localGoalData.threeYearGoalDescription}
+          error={errors.threeYearGoalDescription}
           onChange={handleOnTextFieldChange}
         />
       </Box>
