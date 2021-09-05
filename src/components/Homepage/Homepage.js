4101b3fd91a27b60cbfa21f583b58e1bab7bd9da
Whitney <whitzhu@gmail.com>
2021-09-05 14:01:34 -0700

    Homepage add date

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,3 +1,25 @@
+import { Box, Typography } from "@material-ui/core";
+
 export default function Homepage() {
-  return <div>Homepage</div>;
+  const date = new Date();
+  var options = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+
+  return (
+    <Box display="flex" flexDirection="column">
+      <Box textAlign="center">
+        <Typography variant="h2">
+          {new Intl.DateTimeFormat("en-US", {
+            weekday: "long",
+          }).format(date)}
+        </Typography>
+        <Typography variant="h5">
+          {date.toLocaleDateString("en-US", options)}
+        </Typography>
+      </Box>
+    </Box>
+  );
 }
